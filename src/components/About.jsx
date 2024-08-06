946d5ebb607412a0e5580f014d0d2408418e0094
NIMALKUMARR <nimalarun5@gmail.com>
2024-08-06 05:28:06 -0400

    plt

---
+++ src/components/About.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import {Tilt} from 'react-tilt';
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+I am a dedicated and driven individual with a solid foundation in computer science, bolstered by a Master of Science in Computer Science from the University of North Carolina at Charlotte and a Bachelor of Technology in Computer Science and Engineering from SRM Institute of Science and Technology. My educational journey has equipped me with comprehensive knowledge in various areas such as algorithms, data structures, computer graphics, knowledge discovery in databases, visual analytics, and network-based application development.
+
+My technical skill set includes proficiency in multiple programming languages such as Java, Python, C#, C, C++, JavaScript, and TypeScript. I have hands-on experience with web development technologies, including HTML, CSS, Spring Boot, Node.js, React, and Angular. My database management skills encompass PostgreSQL, MongoDB, and MySQL. Additionally, I am well-versed in cloud and distributed services, particularly with Apache Kafka, Kubernetes, Spinnaker, and Google Cloud Platform (GCP). I am adept at using version control systems like Git to manage and collaborate on projects effectively.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
